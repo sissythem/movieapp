868de45a0ad39429915d371790ab68ab48ba61de
cthem <c.themeli@gmail.com>
2017-11-10 11:44:12 +0200

    Movie rest and Show rest add/remove favorite and reviews

--- GntMovies-ejb/src/main/java/com/gnt/movies/entities/MovieReview.java
+++ GntMovies-ejb/src/main/java/com/gnt/movies/entities/MovieReview.java
@@ -15,7 +15,8 @@ import javax.persistence.*;
         @NamedQuery(name = "MovieReview.findById", query = "SELECT m FROM MovieReview m WHERE m.id = :id"),
         @NamedQuery(name = "MovieReview.findByRating", query = "SELECT m FROM MovieReview m WHERE m.rating = :rating"),
         @NamedQuery(name = "MovieReview.findByUserId", query = "SELECT m FROM MovieReview m WHERE m.user.id = :userId"),
-        @NamedQuery(name = "MovieReview.findByMovieId", query = "SELECT m FROM MovieReview m WHERE m.movie.id = :movieId")
+        @NamedQuery(name = "MovieReview.findByMovieId", query = "SELECT m FROM MovieReview m WHERE m.movie.id = :movieId"),
+        @NamedQuery(name = "MovieReview.findMovieReview", query = "SELECT m FROM MovieReview m WHERE m.movie.id = ?1 AND m.user.id = ?2")
 })
 public class MovieReview implements Serializable {
     private static final long serialVersionUID = 1L;
