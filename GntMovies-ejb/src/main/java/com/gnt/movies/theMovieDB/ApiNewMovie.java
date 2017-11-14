cca2e73c5273e795708ead6fd2d8f9f4e91d5667
cthem <c.themeli@gmail.com>
2017-11-14 16:57:47 +0200

    add posterPath to movies and shows

--- GntMovies-ejb/src/main/java/com/gnt/movies/theMovieDB/ApiNewMovie.java
+++ GntMovies-ejb/src/main/java/com/gnt/movies/theMovieDB/ApiNewMovie.java
@@ -3,86 +3,112 @@ package com.gnt.movies.theMovieDB;
 import com.google.gson.annotations.SerializedName;
 
 public class ApiNewMovie {
-	 @SerializedName("vote_count")
-	 private int voteCount;
-	 @SerializedName("id")
-	 private int id;
-	 @SerializedName("vote_average")
-	 private double voteAverage;
-	 @SerializedName("title")
-	 private String title;
-	 @SerializedName("original_language")
-	 private String originalLanguage;
-	 @SerializedName("original_title")
-	 private String originalTitle;
-	 @SerializedName("adult")
-	 private boolean adult;
-	 @SerializedName("overview")
-	 private String overview;
-	 @SerializedName("release_date")
-	 private String releaseDate;
-	 
+	@SerializedName("vote_count")
+	private int voteCount;
+	@SerializedName("id")
+	private int id;
+	@SerializedName("vote_average")
+	private double voteAverage;
+	@SerializedName("title")
+	private String title;
+	@SerializedName("poster_path")
+	private String poster_path;
+	@SerializedName("original_language")
+	private String originalLanguage;
+	@SerializedName("original_title")
+	private String originalTitle;
+	@SerializedName("adult")
+	private boolean adult;
+	@SerializedName("overview")
+	private String overview;
+	@SerializedName("release_date")
+	private String releaseDate;
+
 	public int getVoteCount() {
 		return voteCount;
 	}
+
 	public void setVoteCount(int voteCount) {
 		this.voteCount = voteCount;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public double getVoteAverage() {
 		return voteAverage;
 	}
+
 	public void setVoteAverage(double voteAverage) {
 		this.voteAverage = voteAverage;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+	public String getPoster_path() {
+		return poster_path;
+	}
+
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+
 	public String getOriginalLanguage() {
 		return originalLanguage;
 	}
+
 	public void setOriginalLanguage(String originalLanguage) {
 		this.originalLanguage = originalLanguage;
 	}
+
 	public String getOriginalTitle() {
 		return originalTitle;
 	}
+
 	public void setOriginalTitle(String originalTitle) {
 		this.originalTitle = originalTitle;
 	}
+
 	public boolean isAdult() {
 		return adult;
 	}
+
 	public void setAdult(boolean adult) {
 		this.adult = adult;
 	}
+
 	public String getOverview() {
 		return overview;
 	}
+
 	public void setOverview(String overview) {
 		this.overview = overview;
 	}
+
 	public String getReleaseDate() {
 		return releaseDate;
 	}
+
 	public void setReleaseDate(String releaseDate) {
 		this.releaseDate = releaseDate;
 	}
+
 	@Override
 	public String toString() {
-		return "ApiNewMovie [voteCount=" + voteCount + ", id=" + id + ", voteAverage=" + voteAverage
-				+ ", title=" + title + ", originalLanguage=" + originalLanguage + ", originalTitle=" + originalTitle
-				+ ", adult=" + adult + ", overview=" + overview + ", releaseDate=" + releaseDate + "]";
+		return "ApiNewMovie [voteCount=" + voteCount + ", id=" + id + ", voteAverage=" + voteAverage + ", title="
+				+ title + ", originalLanguage=" + originalLanguage + ", originalTitle=" + originalTitle + ", adult="
+				+ adult + ", overview=" + overview + ", releaseDate=" + releaseDate + "]";
 	}
-	
-	
 
 }
--- GntMovies-ejb/src/main/java/com/gnt/movies/theMovieDB/ApiNewShow.java
+++ GntMovies-ejb/src/main/java/com/gnt/movies/theMovieDB/ApiNewShow.java
@@ -5,77 +5,104 @@ import java.util.ArrayList;
 import com.google.gson.annotations.SerializedName;
 
 public class ApiNewShow {
-	
+
 	@SerializedName("original_name")
 	private String originalName;
 	@SerializedName("name")
 	private String name;
 	@SerializedName("origin_country")
-    private ArrayList<String> originCountry;
+	private ArrayList<String> originCountry;
 	@SerializedName("vote_count")
-    private int voteCount;
+	private int voteCount;
+	@SerializedName("poster_path")
+	private String poster_path;
 	@SerializedName("first_air_date")
-    private String firstAirDate;
+	private String firstAirDate;
 	@SerializedName("original_language")
-    private String originalLanguage;
+	private String originalLanguage;
 	@SerializedName("id")
-    private int id;
+	private int id;
 	@SerializedName("vote_average")
-    private double voteAverage;
+	private double voteAverage;
 	@SerializedName("overview")
-    private String overview;
-	
+	private String overview;
+
 	public String getOriginalName() {
 		return originalName;
 	}
+
 	public void setOriginalName(String originalName) {
 		this.originalName = originalName;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public ArrayList<String> getOriginCountry() {
 		return originCountry;
 	}
+
 	public void setOriginCountry(ArrayList<String> originCountry) {
 		this.originCountry = originCountry;
 	}
+
 	public int getVoteCount() {
 		return voteCount;
 	}
+
 	public void setVoteCount(int voteCount) {
 		this.voteCount = voteCount;
 	}
+
+	public String getPoster_path() {
+		return poster_path;
+	}
+
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+
 	public String getFirstAirDate() {
 		return firstAirDate;
 	}
+
 	public void setFirstAirDate(String firstAirDate) {
 		this.firstAirDate = firstAirDate;
 	}
+
 	public String getOriginalLanguage() {
 		return originalLanguage;
 	}
+
 	public void setOriginalLanguage(String originalLanguage) {
 		this.originalLanguage = originalLanguage;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public double getVoteAverage() {
 		return voteAverage;
 	}
+
 	public void setVoteAverage(double voteAverage) {
 		this.voteAverage = voteAverage;
 	}
+
 	public String getOverview() {
 		return overview;
 	}
+
 	public void setOverview(String overview) {
 		this.overview = overview;
 	}
