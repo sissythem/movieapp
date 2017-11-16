f8b0d2c5529b52fbfaee3b0dc7ed4322b6ae5036
cthem <c.themeli@gmail.com>
2017-11-16 11:18:40 +0200

    user bean test

--- GntMovies-ejb/src/test/java/com/gnt/test/beans/TestUserBean.java
+++ GntMovies-ejb/src/test/java/com/gnt/test/beans/TestUserBean.java
@@ -1,31 +1,44 @@
 package com.gnt.test.beans;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.gnt.movies.beans.UserBean;
-import com.gnt.movies.dao.UserDaoImpl;
-import com.gnt.movies.entities.User;
+import com.gnt.movies.dao.UserDao;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestUserBean {
 
 	@Mock 
-	UserDaoImpl userDaoImplMock;
+	UserDao userDaoMock;
+	
+	@Mock
+	EntityManager entityManager;
 	
 	@InjectMocks
 	UserBean userBean;
+
+	@Test
+	public void testLoginUser() {
+		when(userDaoMock.checkCredentials(userBean, "username", "password"))
+			.thenReturn(false);
+		assertEquals(false, userBean.loginUser("username", "password"));
+		when(userDaoMock.checkCredentials(userBean, "gmaz", "1234"))
+			.thenReturn(true);
+		assertEquals(true, userBean.loginUser("gmaz", "1234"));
+	}
 	
 	@Test
-	public void testLoginUserFailure() {
-		User user = new User(18, "email@email.com", "name", "lastname", 
-				"password", "username");
-		assertEquals(false, userBean.loginUser(user.getUsername(), 
-				user.getPassword()));
+	public void testLoginToken() {
+		when(userDaoMock.checkToken(userBean, "1234")).thenReturn(true);
+		assertEquals(true, userBean.loginToken("1234"));
 	}
-
 }
