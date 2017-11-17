65897089be89013c2f3d620c2f86e3fd59b8a04c
cthem <c.themeli@gmail.com>
2017-11-17 11:03:25 +0200

    add integration test

---
+++ GntMoviesTesting/src/main/java/com/gnt/movies/testing/EnterpriseTester.java
@@ -0,0 +1,45 @@
+package com.gnt.movies.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gnt.movies.beans.UserBean;
+import com.gnt.movies.entities.User;
+
+@RunWith(Arquillian.class)
+public class EnterpriseTester 
+{
+	@Deployment
+	public static JavaArchive createDeployment() throws IOException 
+	{
+		JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+				.addPackages(true, "com.gnt")
+				.addAsResource("META-INF/persistence.xml");
+				System.out.println(jar.toString(true));
+				return jar;
+	}
+	
+	// Declare your beans here
+	@EJB
+	UserBean userBean;
+
+	// Add your test cases here
+	@Test
+//	@InSequence(1)
+	public void testProximity() {
+		User user = new User(20, "email@email.com", "firstname", "lastname", "password", "username");
+		// Mock your input data and then combine the result with the expected value
+		// Assert.assertEquals(movieBean.isProximityReserved("4417010628"), true);
+		assertEquals(userBean.registerUser(user), true);
+	}
+}
