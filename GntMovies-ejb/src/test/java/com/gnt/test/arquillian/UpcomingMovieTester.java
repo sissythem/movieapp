58fd3a02f948dc901dd7a1c9092e7f1c93f8711f
cthem <c.themeli@gmail.com>
2017-11-21 17:05:00 +0200

    undo deployment changes

--- GntMovies-ejb/src/test/java/com/gnt/test/arquillian/UpcomingMovieTester.java
+++ GntMovies-ejb/src/test/java/com/gnt/test/arquillian/UpcomingMovieTester.java
@@ -8,7 +8,6 @@ import javax.ejb.EJB;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Assert;
@@ -17,7 +16,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gnt.movies.beans.GenreBean;
-import com.gnt.movies.beans.SchedulerBean;
 import com.gnt.movies.beans.UpcomingMovieBean;
 import com.gnt.movies.entities.Genre;
 import com.gnt.movies.entities.UpcomingMovie;
@@ -31,27 +29,15 @@ public class UpcomingMovieTester {
 	@Deployment
 	public static WebArchive createDeployment() throws IOException {
 
-		WebArchive war = ShrinkWrap.create(WebArchive.class, "testUpcomingMovieBean.war")
+		return ShrinkWrap.create(WebArchive.class, "test.war")
 				.addPackages(true, "com.gnt.movies")
-	            .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-	            .addAsWebInfResource("test-ds.xml")
-	            .addAsLibraries(
+				.addAsLibraries(
 						Maven.resolver().resolve("com.google.code.gson:gson:2.8.2").withoutTransitivity().asFile())
 				.addAsLibraries(
 						Maven.resolver().resolve("com.squareup.okio:okio:1.13.0").withoutTransitivity().asFile())
 				.addAsLibraries(
 						Maven.resolver().resolve("com.squareup.okhttp3:okhttp:3.9.0").withoutTransitivity().asFile())
-	            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-//		return ShrinkWrap.create(WebArchive.class, "test.war")
-//				.addPackages(true, "com.gnt.movies")
-//				.addAsLibraries(
-//						Maven.resolver().resolve("com.google.code.gson:gson:2.8.2").withoutTransitivity().asFile())
-//				.addAsLibraries(
-//						Maven.resolver().resolve("com.squareup.okio:okio:1.13.0").withoutTransitivity().asFile())
-//				.addAsLibraries(
-//						Maven.resolver().resolve("com.squareup.okhttp3:okhttp:3.9.0").withoutTransitivity().asFile())
-//				.addAsResource("META-INF/persistence.xml");
-		return war;
+				.addAsResource("META-INF/persistence.xml");
 	}
 	
 	@EJB
