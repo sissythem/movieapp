d80e2da8230252d2ab0577dde579f2018aceb079
cthem <c.themeli@gmail.com>
2017-12-07 19:31:05 +0200

    add toolbar to main activity

--- MoviesApp/app/src/main/java/com/gnt/moviesapp/HomeActivity.java
+++ MoviesApp/app/src/main/java/com/gnt/moviesapp/HomeActivity.java
@@ -2,12 +2,46 @@ package com.gnt.moviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.menuToolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                break;
+            case R.id.movies:
+                break;
+            case R.id.shows:
+                break;
+            case R.id.user:
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
