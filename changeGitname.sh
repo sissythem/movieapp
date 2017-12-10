463aa0c7ce9cbeb79ad798e01828aa50774f0b9b
cthem <c.themeli@gmail.com>
2017-12-10 13:18:01 +0200

    small changes

--- changeGitname.sh
+++ changeGitname.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="c.themeli@gnt.gr"          
+OLD_NAME="usw071"          
 CORRECT_NAME="cthem"            
 CORRECT_EMAIL="c.themeli@gnt.gr"              
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
